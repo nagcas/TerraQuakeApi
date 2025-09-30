ac082810d29c6d86444c5ad1c75d48b1ed1b255e
Nishant Raj <rnishant721@gmail.com>
2025-09-30 22:51:34 +0530

    made a new reponsive new home component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -67,7 +67,7 @@ function App() {
                 path="/terms-and-conditions"
                 element={<TermsAndConditions />}
               />
-              {/* <Route path='/privacy-policy' element={<PrivacyPolicy />} /> */}
+              <Route path="/privacy-policy" element={<PrivacyPolicy />} />
               <Route path="*" element={<NoPage />} />
             </Routes>
             <Footer />
