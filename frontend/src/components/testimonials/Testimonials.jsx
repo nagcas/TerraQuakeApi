9997e33f0794d974777005988cb19eb29eaf4679
AnilKumt <anilkumt51@gmail.com>
2025-10-03 08:32:17 +0530

    made testimonial component

---
+++ frontend/src/components/testimonials/Testimonials.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ClientFeedback from '../ui/testimonial'
+
+const Testimonials = () => {
+  return (
+    <div className='w-full h-screen'>
+        <ClientFeedback/>
+    </div>
+  )
+}
+
+export default Testimonials
