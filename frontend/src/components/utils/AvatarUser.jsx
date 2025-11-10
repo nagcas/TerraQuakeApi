e3fe2386860cce943f1e8f546556485054782100
nagcas <studio.nagcas@outlook.it>
2025-11-10 17:59:46 +0100

    feat: Add testimonials section and fix navbar menu
    - Adds a new "Testimonials" component to the homepage to display user feedback and increase trust in the service.
    - Fixes a bug in the navbar where the user profile dropdown menu would not close correctly after clicking one of its items.

--- frontend/src/components/utils/AvatarUser.jsx
+++ frontend/src/components/utils/AvatarUser.jsx
@@ -2,20 +2,21 @@ import Avatar from '@/assets/images/avatar_default.png';
 import { useContext } from 'react';
 import { Context } from '../modules/Context';
 
-export default function AvatarUser({ use }) {
+export default function AvatarUser({ use, size = '150px' }) {
   const { userLogin } = useContext(Context);
 
   return use === 'navbar' ? (
     <img
       src={userLogin?.avatar || Avatar}
       alt='avatar'
-      className='w-7 h-7 rounded-full cursor-pointer border border-purple-500/30'
+      className='w-7 h-7 rounded-full cursor-pointer border-1 border-purple-500/30'
     />
   ) : (
     <img
       src={userLogin?.avatar || Avatar}
       alt='avatar'
-      className='w-28 h-28 sm:w-36 sm:h-36 rounded-full border-4 border-pink-500 shadow-[0_0_25px_rgba(236,72,153,0.6)] object-cover'
+      style={{ width: size, height: size }}
+      className='rounded-full border-2 border-pink-500 shadow-[0_0_25px_rgba(236,72,153,0.6)] object-cover'
     />
   );
 }
