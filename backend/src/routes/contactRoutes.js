b1b88be051711e88b56ffba7913d6a1bfc7d19e5
nagcas <studio.nagcas@outlook.it>
2026-01-14 22:16:53 +0100

    feat: simplify contact routes by updating endpoint paths for consistency

--- backend/src/routes/contactRoutes.js
+++ backend/src/routes/contactRoutes.js
@@ -17,18 +17,18 @@ import { adminMiddleware } from '../middleware/adminMiddlewares.js'
 const router = express.Router()
 
 // NOTE: Defining a [POST] route to send a new message
-router.post('/create-contact', validatorContact, createContact({ Contact, sendEmailConfirmContact, buildResponse, handleHttpError }))
+router.post('/', validatorContact, createContact({ Contact, sendEmailConfirmContact, buildResponse, handleHttpError }))
 
 // NOTE: Defining a [GET] route to display all received messages
-router.get('/list-all-contacts', adminMiddleware, listAllContacts({ Contact, buildResponse, handleHttpError }))
+router.get('/', adminMiddleware, listAllContacts({ Contact, buildResponse, handleHttpError }))
 
 // NOTE: Defining a [GET] route to display a single message with a specific id
-router.get('/list-one-contact/:id', adminMiddleware, listOneContact({ Contact, buildResponse, handleHttpError }))
+router.get('/:id', adminMiddleware, listOneContact({ Contact, buildResponse, handleHttpError }))
 
 // NOTE: Defining a [PATCH] route to reply to a message with a specific id
-router.patch('/answer-contact/:id', adminMiddleware, validatorAnswer, answerContact({ Contact, sendEmailConfirmAnswer, buildResponse, handleHttpError }))
+router.patch('/:id', adminMiddleware, validatorAnswer, answerContact({ Contact, sendEmailConfirmAnswer, buildResponse, handleHttpError }))
 
 // NOTE: Defining a [DELETE] route to delete a single message with a specific id
-router.delete('/delete-contact/:id', adminMiddleware, deleteContact({ Contact, buildResponse, handleHttpError }))
+router.delete('/:id', adminMiddleware, deleteContact({ Contact, buildResponse, handleHttpError }))
 
 export default router
