f0d0892f55c25b6878a41b160477be79a4717590
nagcas <studio.nagcas@outlook.it>
2026-05-09 19:57:44 +0200

    feat: add translation support for "Page Not Found" in NoPage component

--- frontend/src/pages/noPage/NoPage.jsx
+++ frontend/src/pages/noPage/NoPage.jsx
@@ -23,7 +23,7 @@ export default function NoPage() {
       <section className='z-30 w-full min-h-screen flex flex-col items-center justify-center text-center px-6 py-20 bg-gradient-to-b text-white'>
         <TbError404 className='text-9xl text-purple-600' />
         <h1 className='text-3xl md:text-6xl lg:text-9xl mx-auto font-extrabold leading-tight mt-[50px] select-none'>
-          Page Not Found
+          {t('no_page.page_not_found')}
         </h1>
         <p className='mt-6 mx-auto md:text-xl text-gray-300'>
           {t('no_page.description')}
