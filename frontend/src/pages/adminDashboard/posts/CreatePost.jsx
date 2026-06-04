572f56324fac33e15f65125583cf6bb5d53e30f2
nagcas <studio.nagcas@outlook.it>
2026-06-04 14:00:46 +0200

    fix: set default value of published field to false in CreatePost component

--- frontend/src/pages/adminDashboard/posts/CreatePost.jsx
+++ frontend/src/pages/adminDashboard/posts/CreatePost.jsx
@@ -53,7 +53,7 @@ export default function CreatePost({ setPosts }) {
       content: '',
       readTime: '',
       tags: [],
-      published: '',
+      published: false,
     });
   }, [reset]);
 
